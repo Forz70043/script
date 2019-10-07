2bbb38215f61afaf867de49c477591cb74846eb8
Forz <alfonso.pisicchio@libero.it>
2019-10-07 16:49:35 +0200

    add reset form

--- base.js
+++ base.js
@@ -42,3 +42,41 @@ function disable(e){
 	return false;
 };
 
+
+
+function resetForm(el) {
+    // variable declaration
+    var x, y, z, type = null, object = [];
+    // collect form elements
+    object[0] = document.getElementById(el).getElementsByTagName('input');
+    object[1] = document.getElementById(el).getElementsByTagName('textarea');
+    object[2] = document.getElementById(el).getElementsByTagName('select');
+    // loop through found form elements
+    for (x = 0; x < object.length; x++) {
+        for (y = 0; y < object[x].length; y++) {
+            // define element type
+            type = object[x][y].type;
+            switch (type) {
+            case 'text':
+            case 'textarea':
+            case 'password':
+                object[x][y].value = '';
+                break;
+            case 'radio':
+            case 'checkbox':
+                object[x][y].checked = '';
+                break;
+            case 'select-one':
+                object[x][y].options[0].selected = true;
+                break;
+            case 'select-multiple':
+                for (z = 0; z < object[x][y].options.length; z++) {
+                    object[x][y].options[z].selected = false;
+                }
+                break;
+            } // end switch
+        } // end for y
+    } // end for x
+};
+
+
