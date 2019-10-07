e3a6b46d1c77520b473245a435d9e6805591156d
Forz <alfonso.pisicchio@libero.it>
2019-10-07 16:31:59 +0200

    add base.js
    utils js

---
+++ base.js
@@ -0,0 +1,28 @@
+/* JS Library */
+
+function _e(elem){
+	if(typeof(elem)=='object') return elem;
+	else return document.getElementById(elem);
+};
+
+function show(e,display){
+	if(typeof(display)==='undefined') display='';
+	if((e=_e(e))) {
+		if(display!==false) e.style.display=display;
+		e.style.visibility='visible';
+		return true;
+	}
+	return false;
+};
+
+function hide(e,display){
+	if(typeof(display)==='undefined') display='none';
+	if((e=_e(e))) {
+		if(display!==false) e.style.display=display;
+		e.style.visibility='hidden';
+		return true;
+	}
+	return false;
+};
+
+
