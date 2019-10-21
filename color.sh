ca4c42442ed0221e90808ed65cab65c4e282a316
Forz <alfonso.pisicchio@libero.it>
2019-10-21 15:02:41 +0200

    added colored notice

---
+++ color.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /home/Project/script/setAsset.sh
+
+e_header "Sono un semplice script"
+e_success "Sono un messaggio di Successo"
+e_error "Sono un messaggio di Errore"
+e_warning "Io sono un messaggio di Avvertimento"
+e_underline "Sono un messaggio di testo sottolineato"
+e_bold "Sono un testo in grassetto"
+e_note "e io sono una Nota"
+
+
