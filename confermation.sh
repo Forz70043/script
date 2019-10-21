87fc193b3c898e459f985fe6ccc675d957042966
Forz <alfonso.pisicchio@libero.it>
2019-10-21 15:07:32 +0200

    added func is_confirmed

---
+++ confermation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /home/Project/script/setAsset.sh
+
+seek_confirmation() {
+    printf "\n${bold}$@${reset}"
+    read -p " (y/n) " -n 1
+    printf "\n"
+}
+
+# Test whether the result of an 'ask' is a confirmation
+is_confirmed() {
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        return 0
+    fi
+        return 1
+}
