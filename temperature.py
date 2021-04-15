7c3f83ce4f9a4f1eab4cd328b76270ec4422c91b
Forz70043 <alfonso.pisicchio@libero.it>
2021-04-15 12:14:24 +0200

    Temperature script with python

---
+++ temperature.py
@@ -0,0 +1,12 @@
+
+
+temperatura = float(input("Temperatura: "))
+
+
+if temperatura > 37:
+    print("Hai la febbre\nRiposati")
+elif temperatura < 36:
+    print("Temperatura bassa")
+else:
+    print("Stai bene")
+
